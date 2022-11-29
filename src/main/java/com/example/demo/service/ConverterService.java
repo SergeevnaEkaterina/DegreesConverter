318e4edee753961e72d9c908592a81820e3a5364
Ekaterina Gerasina <gerasinaes@mail.ru>
2022-11-29 20:33:32 +0300

    add array conversion closes #3

--- src/main/java/com/example/demo/service/ConverterService.java
+++ src/main/java/com/example/demo/service/ConverterService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +17,31 @@ public class ConverterService {
     public static String processFarenheitToCelcium(String temperature, HttpServletResponse response) {
         return validateInputTemperature(temperature, convertToCelcium(temperature), response);
     }
+
+    public static List<String> processFarenheitToCelciumArray(List<String> temperatureArray, HttpServletResponse response) {
+        List<String> convertedArray = new ArrayList<>();
+        String validation = validateInputArrayTemperature(temperatureArray);
+        if (validation == null) {
+            temperatureArray.forEach(t -> convertedArray.add(convertToCelcium(t)));
+        } else {
+            response.setStatus(400);
+            convertedArray.add(validation);
+        }
+        return convertedArray;
+    }
+
+    public static List<String> processCelciumToFarenheitArray(List<String> temperatureArray, HttpServletResponse response) {
+        List<String> convertedArray = new ArrayList<>();
+        String validation = validateInputArrayTemperature(temperatureArray);
+        if (validation == null) {
+            temperatureArray.forEach(t -> convertedArray.add(convertToFarenheit(t)));
+        } else {
+            response.setStatus(400);
+            convertedArray.add(validation);
+        }
+        return convertedArray;
+    }
+
 
     public static String convertToFarenheit(String temperature) {
         return String.valueOf((1.8 * Double.parseDouble(temperature)) + 32);
@@ -32,6 +59,18 @@ public class ConverterService {
             result = "Invalid temperature format";
             response.setStatus(400);
         } else result = convertedTemperature;
+        return result;
+    }
+
+    public static String validateInputArrayTemperature(List<String> temperatureArray) {
+        String result = null;
+        Pattern p = Pattern.compile("(-)?\\d+.\\d+");
+        for (String elem : temperatureArray) {
+            Matcher matcher = p.matcher(elem);
+            if (!matcher.matches()) {
+                result = "Invalid temperature format";
+            }
+        }
         return result;
     }
 
